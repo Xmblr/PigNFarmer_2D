fc25ebbc688855dd2168312f11a8c45c98be4186
Xmblr <RbhbkkLoveKate14>
2021-08-27 19:20:28 +0300

    Player movement include

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -6,22 +6,85 @@ public class PlayerScript : MonoBehaviour
 {
     public GameObject Player;
     public GameObject TargetPoints;
-    public GameObject[] Arr;
+    private GameObject[,] arrayOfPoints = new GameObject[5, 9];
+    private int CurrentI;
+    private int CurrentJ;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+
+
+        // move to strat point
         Player.transform.position = TargetPoints.transform.GetChild(0).position;
-        
+        CurrentI = 2;
+        CurrentJ = -1;
+
+        // set multidimensional array of points
+        int k = 1;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                arrayOfPoints[i, j] = TargetPoints.transform.GetChild(k).gameObject;
+                k++;
+            }
+        }
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        transform.position = Vector3.MoveTowards(transform.position, Movement(), Time.deltaTime * 5 );
+
+    }
+
+    Vector3 Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (CurrentI > 0)
+            {
+                CurrentI--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (CurrentI < 4)
+            {
+                CurrentI++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (CurrentJ > 0)
+            {
+                CurrentJ--;
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Player.transform.Translate(Arr[1].transform.position * 0.01f * Time.deltaTime);
+            if (CurrentJ < 8)
+            {
+                CurrentJ++;
+            }
         }
+
+        return arrayOfPoints[CurrentI, CurrentJ].transform.position;
+
+
 
     }
 }
+
+
+
